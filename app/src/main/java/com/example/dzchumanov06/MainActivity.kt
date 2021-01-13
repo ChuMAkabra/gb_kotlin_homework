aa58399c9a222fe87a496291199310855917fbd0
Dmitry Chumanov <dmitry.chumanov@gmail.com>
2021-01-13 18:58:14 +0300

    3) Button added. Once pushed, the button disappears and changes the main TextView text

--- app/src/main/java/com/example/dzchumanov06/MainActivity.kt
+++ app/src/main/java/com/example/dzchumanov06/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.dzchumanov06
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvHello = findViewById<TextView>(R.id.tvHello)
+        val btnPushMe = findViewById<Button>(R.id.btnPushMe)
+
+        btnPushMe.setOnClickListener {
+            tvHello.text = getString(R.string.button_pushed)
+            it.visibility = View.INVISIBLE
+        }
     }
 }

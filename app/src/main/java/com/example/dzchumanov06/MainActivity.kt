b9ffa529b5ee69fd268eda77c85e0eb89f57f122
Dmitry Chumanov <dmitry.chumanov@gmail.com>
2021-04-02 19:16:26 +0300

    1) Created a new Fragment + ViewModel project from scratch

--- app/src/main/java/com/example/dzchumanov06/MainActivity.kt
+++ app/src/main/java/com/example/dzchumanov06/MainActivity.kt
@@ -1,22 +1,18 @@
 package com.example.dzchumanov06
 
-import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dzchumanov06.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val tvHello = findViewById<TextView>(R.id.tvHello)
-        val btnPushMe = findViewById<Button>(R.id.btnPushMe)
-
-        btnPushMe.setOnClickListener {
-            tvHello.text = getString(R.string.button_pushed)
-            it.visibility = View.INVISIBLE
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
         }
     }
 }
---
+++ app/src/main/java/com/example/dzchumanov06/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.dzchumanov06.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

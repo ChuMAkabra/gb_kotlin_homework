7089396d293707b6104cc983c9c3b2a89a8bb971
Dmitry Chumanov <dmitry.chumanov@gmail.com>
2021-04-04 21:01:11 +0300

    5) Dummy LiveData change implemented

--- app/src/main/java/com/example/dzchumanov06/view/MainFragment.kt
+++ app/src/main/java/com/example/dzchumanov06/view/MainFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import com.example.dzchumanov06.databinding.MainFragmentBinding
 import com.example.dzchumanov06.viewmodel.MainViewModel
 
@@ -30,8 +33,18 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        // получим экземпляр нашей ViewModel, чтобы фрагмент мог на нее подписаться (вернее на ее LiveData)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        // создаем Наблюдателя - вызывает события при обновлении данных в LiveData
+        val observer = Observer<Any> { renderData(it) }
+        // подписываемся на LiveData
+        // viewLifecycleOwner - помогает следить за изменением состояния Activity или Fragment
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+    }
+
+    // обработка данных при их изменении
+    fun renderData(liveData: Any) {
+        Toast.makeText(context, "dummy data presented", Toast.LENGTH_LONG).show()
     }
 
     // во избежание утечек и нежелаемого поведения, обнулим _binding (в активити не нужно)
--- app/src/main/java/com/example/dzchumanov06/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/dzchumanov06/viewmodel/MainViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.dzchumanov06.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(private val liveData : MutableLiveData<Any> = MutableLiveData()) : ViewModel() {
+
+    fun getData() : LiveData<Any> {
+        getDataFromLocalSource()
+        return liveData
+    }
+
+    fun getDataFromLocalSource() {
+        Thread {
+            Thread.sleep(1000)
+            liveData.postValue("something")
+        }.start()
+    }
 }

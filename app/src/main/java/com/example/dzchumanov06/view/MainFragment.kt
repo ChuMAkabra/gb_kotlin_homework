3853cfdc836554b4dbbf43c150d987fff5182f98
Dmitry Chumanov <dmitry.chumanov@gmail.com>
2021-04-04 21:47:24 +0300

    8) Changed the appearance of the main fragment

--- app/src/main/java/com/example/dzchumanov06/view/MainFragment.kt
+++ app/src/main/java/com/example/dzchumanov06/view/MainFragment.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.observe
 import com.example.dzchumanov06.databinding.MainFragmentBinding
 import com.example.dzchumanov06.viewmodel.AppState
 import com.example.dzchumanov06.viewmodel.MainViewModel
@@ -45,10 +43,11 @@ class MainFragment : Fragment() {
 
     // обработка данных при их изменении
     fun renderData(liveData: AppState) {
+        binding.tvDetails.visibility = View.VISIBLE
         when(liveData) {
-            is AppState.Success -> binding.message.text = "Your data: ${liveData.weatherData}"
-            is AppState.Error   -> binding.message.text = "Error"
-            AppState.Loading    -> binding.message.text = "Loading, please wait..."
+            is AppState.Success -> binding.tvDetails.text = "Your data: ${liveData.weatherData}"
+            is AppState.Error   -> binding.tvDetails.text = "Error"
+            AppState.Loading    -> binding.tvDetails.text = "Loading, please wait..."
         }
     }
 
